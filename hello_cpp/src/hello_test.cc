6fe3928a766e5df6037d31436a293647d39a6493
xrw67 <r0ngwe1@outlook.com>
2023-06-29 13:35:01 +0800

    add(hello_cpp): add code coverage

--- hello_cpp/src/hello_test.cc
+++ hello_cpp/src/hello_test.cc
@@ -1,12 +1,8 @@
 int Add(int a, int b) { return a + b; }
 
-#if ENABLE_TEST
-
 #include <gmock/gmock.h>
 
 TEST(Hello, should_one_equals_one) {
   ASSERT_EQ(Add(1, 2), 3);
   ASSERT_NE(Add(-1, -2), -1);
 }
-
-#endif
