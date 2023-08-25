b44763a32c44c676c34eeffc1b934e387c1769fd
xrw67 <xrw67@outlook.com>
2023-08-25 19:03:41 +0800

    hello-cpp: add 3rd cxxopts

---
+++ hello-cpp/3rd/fmt/fmt.h
@@ -0,0 +1,20 @@
+/**
+ * fmt 10.1.0
+ * A modern formatting library
+ * website: https://github.com/fmtlib/fmt
+ * api: https://fmt.dev/latest/api.html
+ * Format String Syntax: https://fmt.dev/latest/syntax.html
+ *
+ */
+#ifndef FMT_FMT_H_
+#define FMT_FMT_H_
+
+#define FMT_HEADER_ONLY 1
+
+#include "chrono.h"
+#include "core.h"
+#include "format.h"
+#include "os.h"
+#include "ranges.h"
+
+#endif  // FMT_FMT_H_
--- hello-cpp/src/main.cc
+++ hello-cpp/src/main.cc
@@ -1,6 +1,28 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(int argc, char *argv[]) {
-  printf("Hello World!\n");
+#include "cxxopts.hpp"
+#include "fmt/fmt.h"
+
+int main(int argc, char** argv) {
+  cxxopts::Options options("test", "A brief description");
+
+  options.add_options()("b,bar", "Param bar", cxxopts::value<std::string>())(
+      "d,debug", "Enable debugging",
+      cxxopts::value<bool>()->default_value("false"))(
+      "f,foo", "Param foo", cxxopts::value<int>()->default_value("10"))(
+      "h,help", "Print usage");
+
+  auto result = options.parse(argc, argv);
+
+  if (result.count("help")) {
+    std::cout << options.help() << std::endl;
+    exit(0);
+  }
+  bool debug = result["debug"].as<bool>();
+  std::string bar;
+  if (result.count("bar")) bar = result["bar"].as<std::string>();
+  int foo = result["foo"].as<int>();
+
+  fmt::println("debug: {}, foo: {}, bar: {}", debug, foo, bar);
   return 0;
 }
